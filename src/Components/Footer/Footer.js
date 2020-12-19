88183488cfeaab5923c98ad0686fecf17971507d
Hamza Tasadaq <hamzatasadaq01@gmail.com>
2020-12-20 01:45:03 +0500

    Adding Animation on button

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
                 <div className="container">
                     <h3>Selecao</h3>
                     <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi
-      placeat.</p>
+                        placeat.</p>
                     <div className="social-links">
                         <a href="#" className="twitter"><i class="fab fa-twitter"></i></a>
                         <a href="#" className="facebook"><i class="fab fa-facebook-f"></i></a>
@@ -17,8 +17,9 @@ function Footer() {
                         <a href="#" className="linkedin"><i class="fab fa-linkedin-in"></i></a>
                     </div>
                     <div className="copyright">
-                        © Copyright <strong><span>Selecao</span></strong>. All Rights Reserved
-    </div>
+                        © Copyright <strong><span>Selecao</span></strong>.
+                         All Rights Reserved
+                    </div>
                 </div>
             </footer>
 
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -2,16 +2,27 @@ import React from 'react'
 import './App.css'
 
 
-import useWebAnimations, { zoomInDown } from "@wellyshen/use-web-animations";
+import useWebAnimations, { zoomInDown, pulse } from "@wellyshen/use-web-animations";
 
 function Hero() {
-    const { keyframes, timing } = zoomInDown;
-    const { ref } = useWebAnimations({
-        keyframes,
+    const mainAnimation = zoomInDown;
+    const animationOne = useWebAnimations({
+        keyframes: mainAnimation.keyframes,
         timing: {
-            ...timing,
+            ...mainAnimation.timing,
             delay: 1000, // Delay 1s
-            duration: timing.duration * 0.75, // Speed up the animation
+            duration: mainAnimation.timing.duration * 0.75, // Speed up the animation
+            easing: "ease-in-out",
+        },
+    });
+
+    const buttonAnimation = pulse;
+    const animationTwo = useWebAnimations({
+        keyframes: buttonAnimation.keyframes,
+        timing: {
+            ...buttonAnimation.timing,
+            delay: 1000, // Delay 1s
+            iterations: Infinity,
             easing: "ease-in-out",
         },
     });
@@ -20,14 +31,14 @@ function Hero() {
         <>
             <section id="hero" className="d-flex flex-column justify-content-end align-items-center">
                 <div id="heroCarousel" data-bs-interval={5000} className="container carousel carousel-fade" data-bs-ride="carousel">
-                    <div ref={ref} className="carousel-item active">
+                    <div ref={animationOne.ref} className="carousel-item active">
                         <div className="carousel-container">
                             <h2 className="animate__animated animate__fadeInDown">Welcome to <span>Selecao</span></h2>
                             <p className="animate__animated fanimate__adeInUp">Ut velit est quam dolor ad a aliquid qui aliquid.
                             Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut.
                             Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore
-          modi architecto.</p>
-                            <a href="#about" className="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
+                            modi architecto.</p>
+                            <a ref={animationTwo.ref} href="#" className="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
                         </div>
                     </div>
                 </div>
